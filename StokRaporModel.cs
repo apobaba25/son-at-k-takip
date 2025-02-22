567597ddc36356b7ad0e076470d43c504c892c34
apobaba25 <abdullahemecc@gmail.com>
2025-02-22 04:29:42 +0300

    Delete StokRaporModel.cs

--- StokRaporModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace son_atik_takip.Models
-{
-    public class StokRaporModel
-    {
-        public string Urun { get; set; }
-        public decimal ToplamMiktar { get; set; }
-        public decimal OrtalamaFiyat { get; set; }
-    }
-}
